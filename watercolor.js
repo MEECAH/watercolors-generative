508df8cd2efeb7f033b3b8053950423a7eb31431
MEECAH <micahjhaycraft@gmail.com>
2020-12-22 12:41:01 -0500

    messing around, added support for seeded randomizations

--- watercolor.js
+++ watercolor.js
@@ -1,23 +1,33 @@
 //some global variables
-var sides, r, recurrences
+var sides, r, recurrences;
 var vertices = [];
-var m = 1
-var sd = 1
+var m = 1;
+var sd = 1;
+var imageScale = 1;
+var seed = Math.round(Math.random()*Number.MAX_SAFE_INTEGER)
+
+let seedtext = document.getElementById("seed")
+seedtext.innerHTML = `
+<p>
+seed value: `+seed+` 
+</p>
+`
 
 //edit this to increase the number of recursions
 //on the deformation function
-var numDeformationLayers = 4;
-
+var numDeformationLayers = 1;
 
 function setup() {
   //frameRate(1)
-  let myCanvas = createCanvas(1100, 990);
+  let myCanvas = createCanvas(imageScale * 1100, imageScale * 990);
   background(35);
   angleMode(DEGREES);
   myCanvas.parent("container");
 
+  randomSeed(seed)
+
   sides = 360 / 10;
-  r = 200;
+  r = imageScale * 280;
 
   // this generates a set of (x,y)
   // coordinates that define a 10 sided ellipse like polygon
@@ -46,7 +56,7 @@ function draw() {
   noFill();
 
   beginShape();
-  //vertices.forEach((vert) => vertex(vert[0], vert[1]));
+  vertices.forEach((vert) => vertex(vert[0], vert[1]));
   endShape();
   //console.log(vertices);
 
@@ -68,15 +78,14 @@ function draw() {
 
 //custom function to deform by add 'jutting' between polygon edges
 function deform(vertices) {
+  push();
+  stroke(255, 0, 0);
+  strokeWeight(3);
 
-
-  for (let i = 0; i < vertices.length-1; i+=2) {
-
-    //stroke(255, 0, 0);
-    //strokeWeight(3);
-
-    midpoint = calcMidpoint(vertices[i], vertices[i+1]);
-    //point(midpoint);
+  for (let i = 0; i < vertices.length - 1; i += 2) {
+    midpoint = calcMidpoint(vertices[i], vertices[i + 1]);
+    midpoint.add(randomGaussian(1, 40)); //this adds imperfection to the midpoint location
+    point(midpoint);
 
     angleMode(RADIANS);
 
@@ -84,11 +93,11 @@ function deform(vertices) {
     //drawArrow(base,midpoint,'red')
 
     vect = createVector(
-      vertices[i+1][0] - vertices[i][0],
-      vertices[i+1][1] - vertices[i][1]
+      vertices[i + 1][0] - vertices[i][0],
+      vertices[i + 1][1] - vertices[i][1]
     );
-    vect.mult(Math.random())// / (numDeformationLayers - 1));
-    //vect.setMag(vect.mag()/numDeformationLayers)
+    //vect.mult(Math.random()); // / (numDeformationLayers - 1));
+    vect.setMag(vect.mag() / (1 * numDeformationLayers));
     vect.rotate(PI / 2);
     vect.add(midpoint);
 
@@ -97,20 +106,23 @@ function deform(vertices) {
 
     jut = [vect.x, vect.y];
     //console.log(jut);
-    vertices.splice(i+1, 0, jut);
+    vertices.splice(i + 1, 0, jut);
     //i++;
 
     //drawArrow(base,vect,'blue')
   }
 
+  pop();
+
   recurrences++;
   if (recurrences == numDeformationLayers) {
-    return vertices
+    return vertices;
   } else {
     return deform(vertices);
   }
 }
 
+//calculate the midpoint between two vertices in a 2d pixel plane
 function calcMidpoint(v1, v2) {
   let midpoint = createVector(0, 0);
   midpoint.add(v1);
