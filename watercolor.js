5d25e282284dd7aa3ad0cdbdaf799b9053aecbe2
MEECAH <micahjhaycraft@gmail.com>
2021-01-04 22:14:11 -0500

    successfully making splotches
    need to make edges more uniform
    
    also added support for rendering gifs

--- watercolor.js
+++ watercolor.js
@@ -1,31 +1,42 @@
 //some global variables
-var seed, sides, r, recurrences, mpVariance, angleVariance, magVariance;
+var seed,
+  sides,
+  r,
+  recurrences,
+  mpVariance,
+  angleVariance,
+  magVariance,
+  numDesiredPaintLayers,
+  numPaintLayersDrawn;
 var vertices = [];
+var ogPolygon = [];
 var m = 1;
 var sd = 1;
 var imageScale = 1;
+var frameCount = 1; //set to 0 for gif creation, else set to 1
 
-//seed = Math.round(Math.random() * Number.MAX_SAFE_INTEGER); //random seed
-seed = 3693199348549950; //use this line for a specific seed
+seed = Math.round(Math.random() * Number.MAX_SAFE_INTEGER); //random seed
+//seed = 3693199348549950; //use this line for a specific seed
 
 //edit this to increase the number of recursions
 //on the deformation function
-var numDeformationLayers = 8;
+var numDeformationLayers = 5;
 
 //choose a basic drawing mode, 0 for lines 1 for color fill
 let drawingMode = 1;
 
 function setup() {
   //frameRate(1)
-  let myCanvas = createCanvas(imageScale * 1100, imageScale * 950);
-  background(35);
+  //createLoop({duration:10, gif:true})
+  let myCanvas = createCanvas(imageScale * 1100, imageScale * 930);
+  background(215);
   angleMode(DEGREES);
   myCanvas.parent("container");
 
   randomSeed(seed);
 
   sides = 360 / 10;
-  r = imageScale * 200;
+  r = imageScale * 160;
 
   // this generates a set of (x,y)
   // coordinates that define a 10 sided ellipse like polygon
@@ -35,6 +46,7 @@ function setup() {
     let y = r * cos(a + 18) + height / 2;
     vert = [x, y];
     vertices.push(vert);
+    ogPolygon.push(vert);
     //xCoords.push(x)
     //yCoords.push(y)
     //console.log(vert)
@@ -47,42 +59,71 @@ function setup() {
 
 function draw() {
 
-  if(drawingMode == 0){
+  
+
+  if (drawingMode == 0) {
     stroke(255);
     noFill();
-  }else{
-    fill(237, 34, 93);
+  } else {
+    fill(237, 34, 93,6);
     noStroke();
   }
 
-  //base polygon before any deformations
-  beginShape();
-  //vertices.forEach((vert) => vertex(vert[0], vert[1]));
-  endShape();
-  //console.log(vertices);
+  for(let i = 0; i<50; i++){
+    splotch();
+    vertices = [];
+    vertices = [...ogPolygon]
+  }
 
-  center = createVector(width / 2, height / 2);
-
-  recurrences = 0;
-  let deformed = deform(vertices);
-
-  //recursively deformed polygon
-  beginShape();
-  deformed.forEach((vert) => vertex(vert[0], vert[1]));
-  endShape();
-
-  //draw red stuff
-  stroke(255, 0, 0);
-  strokeWeight(5);
+  // if (frameCount == 5) {
+  //   clear();
+  //   background(35);
+  //   frameCount = 0;
+  //   numDeformationLayers = 1;
+  //   vertices = [];
+  //   vertices = [...ogPolygon];
+  // } //code block above only used for animated gif rendering
 
   noLoop();
+  //loop();
+}
+
+function splotch() {
+  //this if/else is for making gifs of the deformation process
+  if (frameCount == 0) {
+    //base polygon before any deformations
+    beginShape();
+    vertices.forEach((vert) => vertex(vert[0], vert[1]));
+    endShape();
+    //console.log(vertices);
+
+    frameCount++;
+  } else {
+    center = createVector(width / 2, height / 2);
+
+    recurrences = 0;
+    let deformed = deform(vertices);
+
+    //recursively deformed polygon
+    beginShape();
+    deformed.forEach((vert) => vertex(vert[0], vert[1]));
+    endShape();
+
+    //draw red stuff
+    //stroke(255, 0, 0);
+    //strokeWeight(5);
+
+    //these are exclusively used for making animated gif versions for documentation purposes
+    // frameCount++;
+    // numDeformationLayers++;
+  }
 }
 
 //custom function to deform by add 'jutting' between polygon edges
 function deform(vertices) {
   push();
-  stroke(255, 0, 0);
-  strokeWeight(3);
+  //stroke(255, 0, 0);
+  //strokeWeight(3);
 
   for (let i = 0; i < vertices.length - 1; i += 2) {
     //for fine tuning the randomness on polygon deformation
@@ -103,7 +144,7 @@ function deform(vertices) {
       vertices[i + 1][0] - vertices[i][0],
       vertices[i + 1][1] - vertices[i][1]
     );
-    vect.mult(Math.random()); // / (numDeformationLayers - 1));
+    vect.mult(random()); // / (numDeformationLayers - 1));
     //vect.setMag( vect.mag()*magVariance / (1 * numDeformationLayers)); //add imperfection to the mag of jut
     vect.rotate(90 + angleVariance); //add imperfection to the rotation of the jut
     vect.add(midpoint);
